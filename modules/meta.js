ca458bb52c7921aa2b35bb2f5450015bdd5d3c75
Ubuntu <wavve@dev-E8V3-storagemigration-worker-dev.twewpj2zjpmu5hknvwozbt544d.syx.internal.cloudapp.net>
2019-11-01 05:06:43 +0000

    20191101 1406

--- modules/meta.js
+++ modules/meta.js
@@ -2,7 +2,7 @@
  * @Author: Mathias.Je 
  * @Date: 2019-10-14 17:04:00 
  * @Last Modified by: Mathias.Je
- * @Last Modified time: 2019-11-01 13:58:33
+ * @Last Modified time: 2019-11-01 14:05:33
  */
 import mysql from 'mysql2/promise';
 import container from './logger';
@@ -40,7 +40,7 @@ class Meta {
                     FROM 
                         ${process.env.TABLE_NAME} 
                     WHERE 
-                        isMig = ${process.env.ISMIG} 
+                        isMig = "${process.env.ISMIG}" 
                     ORDER BY 
                         j_UpdatedAt DESC 
                     LIMIT 1 
